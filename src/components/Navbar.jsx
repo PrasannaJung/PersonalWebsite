6f6c238e08fa87db5b4a59e6166d6259923a8504
PrasannaJung <prasannajung08@gmail.com>
2022-10-29 23:56:57 +0545

    minor style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center'>
-      <h1 className='uppercase font-bold text-3xl'>PrasannaJ.</h1>
-      <a className='bg-[#4E41A0] px-4 py-2 rounded-sm font-semibold' href=''>
+      <h1 className='uppercase font-bold text-2xl md:text-3xl'>PrasannaJ.</h1>
+      <a
+        className='bg-[#4E41A0] px-2 md:px-4 py-2 rounded-sm font-semibold text-xs md:text-base'
+        href=''
+      >
         Work with me
       </a>
     </nav>
